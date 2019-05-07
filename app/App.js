c23c6b3d3b6d11056da323eb11b005f86f874d12
John Anthony Rivera <coffeeexistence@users.noreply.github.com>
2019-05-07 01:03:11 -0700

    Get tab-nav working

--- app/App.js
+++ app/App.js
@@ -7,26 +7,13 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import AppStartup from "app/components/ctr.AppStartup";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  }
-});
+import Navigation from "app/components/ctr.Navigation";
 
 export default () => (
-  <View style={styles.container}>
+  <View style={StyleSheet.absoluteFill}>
     <AppStartup />
-    <Text style={styles.welcome}>Welcome to React Native!</Text>
+    <Navigation />
   </View>
 );
--- app/components/ctr.Navigation/index.js
+++ app/components/ctr.Navigation/index.js
@@ -1,1 +1,32 @@
 // @flow
+
+import * as React from "react";
+import { View, Text } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+
+class HomeScreen extends React.Component<void> {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component<void> {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Settings: SettingsScreen
+});
+
+export default createAppContainer(TabNavigator);
